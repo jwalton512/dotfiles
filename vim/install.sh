956037de027c1225734a61ad22eab8a4a68d6e88
Jason Walton <jwalton512@gmail.com>
2019-07-30 10:20:08 -0700

    install vim dependencies

---
+++ vim/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Vim
+#
+# This installs dependencies for vim
+
+# Install vim-plug
+if test ! -e $HOME/.vim/autoload/plug.vim
+then
+  echo "  Installing vim-plug for you."
+
+  curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
