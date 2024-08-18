2088a4d0a60c5bf6a1775a081d07132631b8f9d1
Jason Walton <jason@devhour.co>
2024-08-18 16:34:07 -0700

    update wezterm configuration

--- dots/.config/wezterm/wezterm.lua
+++ dots/.config/wezterm/wezterm.lua
@@ -1,10 +1,21 @@
 local wezterm = require 'wezterm'
 
-local config = {
-    font = wezterm.font 'MesloLGS NF',
-    font_size = 16,
-    color_scheme = 'Gruvbox dark, hard (base16)',
-    audible_bell = 'Disabled',
+local config = {}
+
+config.font = wezterm.font 'MesloLGS NF'
+config.font_size = 16
+config.line_height = 1.2
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+config.audible_bell = 'Disabled'
+config.use_fancy_tab_bar = false
+config.term = 'wezterm'
+
+config.keys = {
+	{
+		key = 't',
+		mods = 'CMD',
+		action = wezterm.action.SpawnCommandInNewTab { cwd = wezterm.home_dir }
+	},
 }
 
 return config
