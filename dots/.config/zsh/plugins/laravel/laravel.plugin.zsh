467c010334ca5a9aa0a329541c131c5938f09d28
Jason Walton <jason@devhour.co>
2024-10-18 22:50:12 -0700

    remove laravel-new function

--- dots/.config/zsh/plugins/laravel/laravel.plugin.zsh
+++ dots/.config/zsh/plugins/laravel/laravel.plugin.zsh
@@ -1,5 +1,5 @@
-fpath=(${0:A:h}/functions $fpath)
-autoload -U $fpath[1]/*(.:t)
+# fpath=(${0:A:h}/functions $fpath)
+# autoload -U $fpath[1]/*(.:t)
 
 alias art="php artisan"
 alias sart="sail artisan"
