fad402685d6506eae0bef51629ad2c26b6be680a
Jason Walton <jason@devhour.co>
2025-06-21 15:01:20 -0700

    update zsh aliases

--- dots/.config/zsh/custom/aliases.zsh
+++ dots/.config/zsh/custom/aliases.zsh
@@ -2,6 +2,9 @@
 alias df='cd "$DOTFILES"'
 alias dfe='cd "$DOTFILES" && ${VISUAL:-${EDITOR:-vim}} .'
 
+# git
+alias gs='gst'
+
 # ssh
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '🔑 Public key copied to clipboard.'"
 
