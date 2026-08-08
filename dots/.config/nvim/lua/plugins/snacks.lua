b61d221e10aec3bc5cafe5175ffb538ae45bba45
Jason Walton <jason@devhour.co>
2026-08-07 23:09:22 -0700

    make lazygit occupy the full terminal

--- dots/.config/nvim/lua/plugins/snacks.lua
+++ dots/.config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,14 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      lazygit = {
+        win = {
+          width = 0,
+          height = 0,
+          border = "none",
+          backdrop = false,
+        },
+      },
       explorer = {
         trash = false,
       },
